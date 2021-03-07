dbd5dacf2e5c9236501325f17d6d78672d6e8a7d
Vinicius Da Silva Cruz <vinicius.da.silva.cruz.sanji@gmail.com>
2021-03-07 17:10:26 -0300

    Project hr-user created

---
+++ hr-user/src/test/java/br/com/study/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.study.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
