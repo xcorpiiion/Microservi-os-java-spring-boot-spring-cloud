95e784076b1eaf4a8ea406f11332d1bc962f1082
Vinicius Da Silva Cruz <vinicius.da.silva.cruz.sanji@gmail.com>
2021-03-07 17:14:11 -0300

    Project hr-user configuring

--- hr-user/hr-user-web/src/main/java/br/com/study/hruser/HrUserApplication.java
+++ hr-user/hr-user-web/src/main/java/br/com/study/hruser/HrUserApplication.java
@@ -2,12 +2,14 @@ package br.com.study.hruser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class HrUserApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HrUserApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HrUserApplication.class, args);
+    }
 
 }
