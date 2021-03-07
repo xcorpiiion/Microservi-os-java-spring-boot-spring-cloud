680b41866735656404985e2b180319fd89962371
Vinicius Da Silva Cruz <vinicius.da.silva.cruz.sanji@gmail.com>
2021-03-07 17:41:00 -0300

    Database seeding

--- hr-user/hr-user-web/src/main/java/br/com/study/hruser/HrUserApplication.java
+++ hr-user/hr-user-web/src/main/java/br/com/study/hruser/HrUserApplication.java
@@ -1,8 +1,11 @@
 package br.com.study.hruser;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableEurekaClient
 @SpringBootApplication
@@ -11,5 +14,4 @@ public class HrUserApplication {
     public static void main(String[] args) {
         SpringApplication.run(HrUserApplication.class, args);
     }
-
 }
